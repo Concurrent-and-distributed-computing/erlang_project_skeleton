9573e0d05e94bc9d84a3e71a3c8fdafc25148230
François-Xavier Wicht <wichtfx@protonmail.ch>
2021-11-05 14:02:33 +0100

    refactoring

--- src/erlang_project_skeleton_app.erl
+++ src/erlang_project_skeleton_app.erl
@@ -10,15 +10,15 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    library:start(),
     Dispatch = cowboy_router:compile([{'_',
-                                       [{"/health", health_route, []},
-                                        {"/book", get_book_route, []},
-                                        {"/books", post_book_route, []}]}]),
+                                       [{"/health", health_route, []}]}]),
     {ok, _} = cowboy:start_clear(http,
                                  [{port, 8080}],
-                                 #{env => #{dispatch => Dispatch}}),
-    % populate database
+                                 #{env => #{dispatch => Dispatch},
+                                   middlewares =>
+                                       [cowboy_router,
+                                        ca_cowboy_middleware,
+                                        cowboy_handler]}),
     erlang_project_skeleton_sup:start_link().
 
 stop(_State) -> ok = cowboy:stop_listener(http).
--- src/routes/health_route.erl
+++ src/routes/health_route.erl
@@ -2,10 +2,28 @@
 
 -export([init/2]).
 
-init(Req0, Opts) ->
-    io:format("GET /health~n", []),
-    Req = cowboy_req:reply(200,
-                           #{<<"content-type">> => <<"application/json">>},
-                           <<"{\"health\": \"ok\"}">>,
-                           Req0),
-    {ok, Req, Opts}.
+-export([allowed_methods/2]).
+
+-export([content_types_provided/2]).
+
+-export([known_methods/2]).
+
+-export([health/2]).
+
+init(Req, State) -> {cowboy_rest, Req, State}.
+
+allowed_methods(Req, State) ->
+    {[<<"GET">>], Req, State}.
+
+content_types_provided(Req, State) ->
+    {[{{<<"application">>, <<"json">>, []}, health}],
+     Req,
+     State}.
+
+health(Req, State) ->
+    Message = {[{health, <<"ok">>}]},
+    {jiffy:encode(Message), Req, State}.
+
+known_methods(Req, State) ->
+    Result = [<<"GET">>],
+    {Result, Req, State}.
